8a93908643f50abb8a7dd45bdceebc549c7cb9fd
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-09 12:00:06 +0530

    cors

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 func main() {
 	app := fiber.New()
+
+	app.Use(cors.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
 	})
 
-	app.Listen(":3000")
+	app.Listen(":8000")
 }
